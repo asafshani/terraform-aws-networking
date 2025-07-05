b0c67de3c2a7160556e099515622b5d7b53bb837
asaf <asafsenator@gmail.com>
2025-07-05 15:33:29 +0300

    updating backend with s3 bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-terraform-state"
+    bucket         = "devops-terraform-state5454"
     key            = "eks/network/terraform.tfstate"
     region         = "il-central-1"
     encrypt        = true
